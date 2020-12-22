41bc7172b64a15d32a7cb04acaa4b5947583e385
Osaze Shears <oshears@vt.edu>
2020-12-22 16:34:20 -0500

    organizing files

---
+++ misc/axi_lite_example/servo_controller_xsim_run.tcl
@@ -0,0 +1,4 @@
+#add_wave {{/servo_controller/minimum_high_pulse_width_ns}} {{/servo_controller/maximum_high_pulse_width_ns}} {{/servo_controller/clk}} {{/servo_controller/rst}} {{/servo_controller/servo_position_input}} {{/servo_controller/servo_control_out}} 
+log_wave *
+run 10 us
+add_wave *
