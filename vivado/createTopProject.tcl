5148e9eb4ea17d4b7d89e20e4c5cb8bac4d006f8
Osaze Shears <oshears@vt.edu>
2020-12-31 13:54:53 -0500

    changed the name of the top level design

--- vivado/createTopProject.tcl
+++ vivado/createTopProject.tcl
@@ -7,7 +7,7 @@ set_property  ip_repo_paths  /home/oshears/Documents/vt/research/code/verilog/ne
 update_ip_catalog
 
 # Create Block Design and Add Zynq
-create_bd_design "design_1"
+create_bd_design "neuromorphic_asic_bridge_system"
 update_compile_order -fileset sources_1
 startgroup
 create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 processing_system7_0
@@ -18,9 +18,21 @@ startgroup
 create_bd_cell -type ip -vlnv user.org:user:neuromorphic_asic_bridge_top:1.0 neuromorphic_asic_br_0
 endgroup
 
-# Add UART to the PS
+# Add UART and USB to the PS
 startgroup
 set_property -dict [list CONFIG.PCW_UART1_PERIPHERAL_ENABLE {1}] [get_bd_cells processing_system7_0]
+endgroup
+startgroup
+set_property -dict [list CONFIG.PCW_USB0_PERIPHERAL_ENABLE {1}] [get_bd_cells processing_system7_0]
+endgroup
+startgroup
+set_property -dict [list CONFIG.PCW_QSPI_GRP_SINGLE_SS_ENABLE {1}] [get_bd_cells processing_system7_0]
+endgroup
+startgroup
+set_property -dict [list CONFIG.PCW_SD0_PERIPHERAL_ENABLE {1} CONFIG.PCW_CAN0_PERIPHERAL_ENABLE {1} CONFIG.PCW_CAN0_CAN0_IO {MIO 46 .. 47} CONFIG.PCW_I2C0_PERIPHERAL_ENABLE {1} CONFIG.PCW_I2C0_I2C0_IO {MIO 50 .. 51}] [get_bd_cells processing_system7_0]
+endgroup
+startgroup
+set_property -dict [list CONFIG.PCW_PRESET_BANK1_VOLTAGE {LVCMOS 1.8V} CONFIG.PCW_ENET0_PERIPHERAL_ENABLE {1} CONFIG.PCW_ENET0_ENET0_IO {MIO 16 .. 27}] [get_bd_cells processing_system7_0]
 endgroup
 
 # Block Automation
@@ -61,8 +73,8 @@ update_compile_order -fileset sources_1
 
 validate_bd_design -force
 
-make_wrapper -files [get_files /home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.srcs/sources_1/bd/design_1/design_1.bd] -top
-add_files -norecurse /home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
+make_wrapper -files [get_files /home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.srcs/sources_1/bd/neuromorphic_asic_bridge_system/neuromorphic_asic_bridge_system.bd] -top
+add_files -norecurse /home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.gen/sources_1/bd/neuromorphic_asic_bridge_system/hdl/neuromorphic_asic_bridge_system_wrapper.v
 
 update_compile_order -fileset sources_1
 
@@ -70,16 +82,16 @@ update_compile_order -fileset sources_1
 read_xdc ../xdc/neuromorphic_asic_bridge_system_constraints.xdc
 
 # Open Elaborated Design
-generate_target all [get_files /home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.srcs/sources_1/bd/design_1/design_1.bd]
-create_ip_run [get_files -of_objects [get_fileset sources_1] /home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.srcs/sources_1/bd/design_1/design_1.bd]
-launch_runs design_1_processing_system7_0_0_synth_1
-wait_on_run design_1_processing_system7_0_0_synth_1
-launch_runs design_1_neuromorphic_asic_br_0_0_synth_1
-wait_on_run design_1_neuromorphic_asic_br_0_0_synth_1
-launch_runs design_1_rst_ps7_0_50M_0_synth_1
-wait_on_run design_1_rst_ps7_0_50M_0_synth_1
-launch_runs design_1_auto_pc_0_synth_1
-wait_on_run design_1_auto_pc_0_synth_1
+generate_target all [get_files /home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.srcs/sources_1/bd/neuromorphic_asic_bridge_system/neuromorphic_asic_bridge_system.bd]
+create_ip_run [get_files -of_objects [get_fileset sources_1] /home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.srcs/sources_1/bd/neuromorphic_asic_bridge_system/neuromorphic_asic_bridge_system.bd]
+launch_runs neuromorphic_asic_bridge_system_processing_system7_0_0_synth_1
+wait_on_run neuromorphic_asic_bridge_system_processing_system7_0_0_synth_1
+launch_runs neuromorphic_asic_bridge_system_neuromorphic_asic_br_0_0_synth_1
+wait_on_run neuromorphic_asic_bridge_system_neuromorphic_asic_br_0_0_synth_1
+launch_runs neuromorphic_asic_bridge_system_rst_ps7_0_50M_0_synth_1
+wait_on_run neuromorphic_asic_bridge_system_rst_ps7_0_50M_0_synth_1
+launch_runs neuromorphic_asic_bridge_system_auto_pc_0_synth_1
+wait_on_run neuromorphic_asic_bridge_system_auto_pc_0_synth_1
 synth_design -rtl -rtl_skip_mlo -name rtl_1
 
 # Run Synthesis
@@ -95,6 +107,16 @@ launch_runs impl_1 -to_step write_bitstream -jobs 16
 wait_on_run impl_1
 
 # Export Hardware for Vitis
-write_hw_platform -fixed -force -file /home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system.xsa
+write_hw_platform -fixed -include_bit -force -file /home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system.xsa
+
+# Generate Output Products
+# generate_target all [get_files  /home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.srcs/sources_1/bd/neuromorphic_asic_bridge_system/neuromorphic_asic_bridge_system.bd]
+# catch { config_ip_cache -export [get_ips -all neuromorphic_asic_bridge_system_processing_system7_0_0] }
+# catch { config_ip_cache -export [get_ips -all neuromorphic_asic_bridge_system_auto_pc_0] }
+# export_ip_user_files -of_objects [get_files /home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.srcs/sources_1/bd/neuromorphic_asic_bridge_system/neuromorphic_asic_bridge_system.bd] -no_script -sync -force -quiet
+# create_ip_run [get_files -of_objects [get_fileset sources_1] /home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.srcs/sources_1/bd/neuromorphic_asic_bridge_system/neuromorphic_asic_bridge_system.bd]
+# launch_runs neuromorphic_asic_bridge_system_processing_system7_0_0_synth_1 -jobs 16
+# export_simulation -of_objects [get_files /home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.srcs/sources_1/bd/neuromorphic_asic_bridge_system/neuromorphic_asic_bridge_system.bd] -directory /home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.ip_user_files/sim_scripts -ip_user_files_dir /home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.ip_user_files -ipstatic_source_dir /home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.ip_user_files/ipstatic -lib_map_path [list {modelsim=/home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.cache/compile_simlib/modelsim} {questa=/home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.cache/compile_simlib/questa} {ies=/home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.cache/compile_simlib/ies} {xcelium=/home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.cache/compile_simlib/xcelium} {vcs=/home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.cache/compile_simlib/vcs} {riviera=/home/oshears/Documents/vt/research/code/verilog/neuromorphic_fpga_bridge/vivado/neuromorphic_asic_bridge_system_project/neuromorphic_asic_bridge_system_project.cache/compile_simlib/riviera}] -use_ip_compiled_libs -force -quiet
+
 
 exit
