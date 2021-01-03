3299489a688f7bfa0fa76d84f11fa162c4aac925
Osaze Shears <oshears@vt.edu>
2021-01-02 22:59:42 -0500

    debugging petalinux build

--- embedded_linux/createLinux.tcl
+++ embedded_linux/createLinux.tcl
@@ -8,6 +8,10 @@ petalinux-config -p neuromorphic_peta_linux --get-hw-description=../vitis/neurom
 
 # In petalinux-config DTG Settings ---> (template) MACHINE_NAME to zedboard
 
+# TODO: Need to debug errors and warnings in build/build.log
 petalinux-build -p neuromorphic_peta_linux
 
-# petalinux-package
+#petalinux-package --boot --format BIN --fsbl <FSBL image> --fpga ../vitis/neuromorphic/export/neuromorphic/hw/neuromorphic_asic_bridge_system_wrapper.bit --u-boot
+
+# TODO
+# petalinux-package --boot --format BIN --fsbl images/linux/zynqmp_fsbl.elf --u-boot images/linux/u-boot.elf --pmufw images/linux/pmufw.elf --fpga images/linux/*.bit --force
