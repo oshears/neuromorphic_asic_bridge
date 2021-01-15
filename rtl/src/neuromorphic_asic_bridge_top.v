9de85c3063ab69ee1eece509deb5151970652358
Osaze Shears <oshears@vt.edu>
2021-01-15 10:01:05 -0500

    updated acquisition time (again)

--- rtl/src/neuromorphic_asic_bridge_top.v
+++ rtl/src/neuromorphic_asic_bridge_top.v
@@ -199,7 +199,7 @@ xadc_interface xadc_interface(
 XADC #(// Initializing the XADC Control Registers
     .INIT_40(16'hB903), // Multiplexer Input on VP/VN Channel, 256 sample averaging and settling (acquisition) time 
     .INIT_41(16'h20F0),// Continuous Seq Mode, Calibrate ADC and Supply Sensor
-    .INIT_42(16'h0F00),// Set DCLK divides
+    .INIT_42(16'h3F00),// Set DCLK divides
     .INIT_49(16'h000F),// CHSEL2 - enable aux analog channels 0 - 3
     .INIT_4B(16'h000F), // enable averaging
     .INIT_4F(16'h000F), // enable settling time
