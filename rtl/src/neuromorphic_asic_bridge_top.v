601790cd9a61229204c300637475e7de28b1ed95
Osaze Shears <oshears@vt.edu>
2021-01-15 08:41:33 -0500

    increased xadc acquisition time

--- rtl/src/neuromorphic_asic_bridge_top.v
+++ rtl/src/neuromorphic_asic_bridge_top.v
@@ -199,10 +199,10 @@ xadc_interface xadc_interface(
 XADC #(// Initializing the XADC Control Registers
     .INIT_40(16'hB903), // Multiplexer Input on VP/VN Channel, 256 sample averaging and settling (acquisition) time 
     .INIT_41(16'h20F0),// Continuous Seq Mode, Calibrate ADC and Supply Sensor
-    .INIT_42(16'h0400),// Set DCLK divides
-    .INIT_49(16'h000f),// CHSEL2 - enable aux analog channels 0 - 3
-    .INIT_4B(16'h000f), // enable averaging
-    .INIT_4F(16'h000f), // enable settling time
+    .INIT_42(16'h0F00),// Set DCLK divides
+    .INIT_49(16'h000F),// CHSEL2 - enable aux analog channels 0 - 3
+    .INIT_4B(16'h000F), // enable averaging
+    .INIT_4F(16'h000F), // enable settling time
     .SIM_MONITOR_FILE("design.txt"),// Analog Stimulus file for simulation
     .SIM_DEVICE("ZYNQ")
 )
